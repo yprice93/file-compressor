f6c7a701188a9298f07c89e948f111680e7b3ab7
yprice93 <yprice93@github.com>
2023-09-10 18:46:38 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import PySimpleGUI as sg
+
+label1 = sg.Text("Select files to compress:")
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Select")
+
+label2 = sg.Text("Select destination folder:")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Select")
+
+compress_button = sg.Button("Compress")
+window = sg.Window("File Compressor",
+                   layout=[[label1, input1, choose_button1],
+                   [label2, input2, choose_button2],
+                           [compress_button]])
+
+window.read()
+window.close()
