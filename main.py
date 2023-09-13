4d869757ecefe56b546759e8c9e97f7a32774018
Yelin Price <yelin.lee.1009@gmail.com>
2023-09-12 19:25:02 -0500

    implement compress feature

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import PySimpleGUI as sg
+from zip_creator import make_archive
 
 label1 = sg.Text("Select files to compress:")
 input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Select")
+choose_button1 = sg.FilesBrowse("Select", key='files')
 
 label2 = sg.Text("Select destination folder:")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Select")
+choose_button2 = sg.FolderBrowse("Select", key='folder')
 
 compress_button = sg.Button("Compress")
 window = sg.Window("File Compressor",
                    layout=[[label1, input1, choose_button1],
-                   [label2, input2, choose_button2],
+                           [label2, input2, choose_button2],
                            [compress_button]])
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+
 window.close()
